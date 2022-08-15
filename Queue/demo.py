ea3dc69b75c8ff3783699428f893bb3d5a2387b5
tus020191 <tus123kumar@gmail.com>
2022-08-15 16:10:35 +0530

    done

--- Queue/demo.py
+++ Queue/demo.py
@@ -1,7 +1,22 @@
-def A(a: list[int])-> None:
+class A:
+    def __init__(self) -> None:
+        self.a=5
+    def f(self,b):
+        tmp=b
+        tmp.a=55
+        #return tmp
+        
+
+
+def fun(a: list[int])-> None:
     pass
 
 l=[5,7]
 k=l.copy()
 print(k)
+
+b=A()
+b.f(b)
+print(b.a)
+
 
