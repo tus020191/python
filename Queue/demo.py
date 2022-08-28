35fc1a3dcfb7f93bf5981c5c47e1b43e5966c49c
tus020191 <tus123kumar@gmail.com>
2022-08-28 21:06:42 +0530

    checked!!!!

--- Queue/demo.py
+++ Queue/demo.py
@@ -1,10 +1,13 @@
+from queue import Queue ,LifoQueue
+from collections import deque
+
 class A:
     def __init__(self) -> None:
         self.a=5
     def f(self,b):
         tmp=b
         tmp.a=55
-        #return tmp
+        return tmp
         
 
 
@@ -16,7 +19,19 @@ k=l.copy()
 print(k)
 
 b=A()
-b.f(b)
+c=b.f(b)
+c.a=89
+
 print(b.a)
 
+q=Queue(maxsize=6)
+d=deque()
+q.put(50)
+print(q.get())
+d.append(45) # right append
 
+d.appendleft(66)# append left
+d.appendleft(666)# append left
+print(d[0])
+print(d[len(d)-1])
+print(d[1])
